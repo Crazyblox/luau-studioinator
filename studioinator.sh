71c41a9db63bf580d073ccbb4b0c2a684f52d384
Crazyblox <47362874+Crazyblox@users.noreply.github.com>
2026-07-29 18:45:24 +0100

    Improved compatibility

--- studioinator.sh
+++ studioinator.sh
@@ -1,47 +1,54 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 # studioinator.sh:
 # Provides luau binaries with Roblox Studio/Playground fflag behaviour, treated as canon by the luau team.
 
-set -e
+set -eu
 
-if [[ "$1" == "--help" ]]; then
-    echo "\nStudioinator - Maps live roblox fflags to luau binaries"
-    echo "\nUsage: ./studioinator.sh [option?] [binary?] [binary options]"
-    echo "\nAvailable options:"
-    echo "--help: Shows this help page"
-    echo "--verbose: Display which fflags and values were passed to the binary"
-    exit 0
-fi
-
+# Perform studioinator options
 VERBOSE=false
-if [[ "$1" == "--verbose" ]]; then
-    VERBOSE=true
+while [ $# -gt 0 ]; do
+    case "${1:-}" in
+        --help)
+            printf '\n%s\n' 'Studioinator - Maps live roblox fflags to luau binaries'
+            printf '\n%s\n' 'Usage: studioinator [studioinator options] [binary] [binary options]'
+            printf '\n%s\n' 'Available options:'
+            printf '%s\t%s\n' '--help:   ' 'Shows this help page'
+            printf '%s\t%s\n' '--verbose:' 'Display which fflags and values were passed to the binary'
+            exit 0
+            ;;
+        --verbose)
+            VERBOSE=true
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+# Accepts provided binary, with fallback to default
+LUAU_PATH=''
+if LUAU_PATH=$(command -v "${1:-}" 2>/dev/null); then
     shift
+elif ! LUAU_PATH=$(command -v luau 2>/dev/null); then
+    printf '%s\n' 'Error: Couldnt resolve binary path' >&2
+    exit 1
 fi
 
-# Assigns default luau binary or provided alternative; done early to prevent fflag warnings slipping through to output
-LUAU_BIN="luau"
-# Accepts LUAU_BIN customisation only if valid.
-if [[ "$1" == *luau* ]] && command -v "$1" >/dev/null 2>&1; then
-    LUAU_BIN="$1"
-    shift
-fi
-
-# Dependency check
-DEPENDENCIES="mktemp grep tr jq curl $LUAU_BIN"
-for arg in $DEPENDENCIES; do
-    if ! command -v $arg >/dev/null 2>&1; then
-        echo "Error: '$arg' not found in PATH" >&2
+# Binary dependencies
+for arg in mktemp grep tr jq curl sort; do
+    if ! command -v "$arg" >/dev/null 2>&1; then
+        printf 'Error: "%s" not found\n' "$arg" >&2
         exit 1
     fi
 done
 
 # Based on luau playground:
 # https://github.com/luau-lang/playground/blob/b0afa61dcf78a12c18d7ab67c44d4a6482aefdec/src/lib/luau/wasm.ts#L25
-FFLAG_URL="https://clientsettingscdn.roblox.com/v1/settings/application?applicationName=PCStudioApp"
+FFLAG_URL='https://clientsettingscdn.roblox.com/v1/settings/application?applicationName=PCStudioApp'
 FFLAG_PREFIXES='["FFlagLuau","FIntLuau","DFFlagLuau","DFIntLuau"]'
-FFLAG_ARGS=$(curl -sSL "$FFLAG_URL"| jq -j --argjson prefixes "$FFLAG_PREFIXES" '
+FFLAG_ARGS=$(curl -fsSL "$FFLAG_URL"| jq -j --argjson prefixes "$FFLAG_PREFIXES" '
     def emit($key; $val):
         # If val is a string, keep it unquoted (e.g., abc)
         # If val is non-string (number/bool/null/object/array), stringify with jq
@@ -65,10 +72,9 @@ FFLAG_ARG_LINES=$(printf '%s\n' "$FFLAG_ARGS" | tr ',' '\n')
 # Perform test to fetch luau fflag warnings
 TEST='return'
 TEMP_TEST=$(mktemp)
-trap 'rm -f "$TEMP_TEST"' EXIT
 printf '%s\n' "$TEST" >"$TEMP_TEST"
 TEST_WARNINGS=$(
-    "$LUAU_BIN" --fflags="false,$FFLAG_ARGS" "$TEMP_TEST" \
+    "$LUAU_PATH" --fflags="false,$FFLAG_ARGS" "$TEMP_TEST" \
         2>&1 >/dev/null || true
 )
 
@@ -82,32 +88,28 @@ FFLAG_INCOMPATIBLE=$(
 
 # Remove lines from FFLAG_ARG_LINES that match with any string from TEMP_INCOMPATIBLE
 TEMP_INCOMPATIBLE=$(mktemp)
-trap 'rm -f "$TEMP_INCOMPATIBLE"' EXIT
 printf '%s\n' "$FFLAG_INCOMPATIBLE" > "$TEMP_INCOMPATIBLE"
+cleanup()
+{
+    rm -f "$TEMP_TEST" "$TEMP_INCOMPATIBLE"
+}
+trap cleanup 0
 FFLAG_FILTERED=$(
     printf '%s\n' "$FFLAG_ARG_LINES" |
     grep -Fv -f "$TEMP_INCOMPATIBLE" || true
 )
 
 # Displays what relevant fflags have been applied from roblox's live fflags.
-if [[ "$VERBOSE" == true ]]; then
-#   printf "TEST_WARNINGS lines: "
-#   printf '%s\n' "$TEST_WARNINGS" | wc -l
-#
-#   printf "FFLAG_ARG_LINES lines: "
-#   printf '%s\n' "$FFLAG_ARG_LINES" | wc -l
-#
-#   printf "TEMP_INCOMPATIBLE lines: "
-#   wc -l < "$TEMP_INCOMPATIBLE"
-#
-#   printf "FFLAG_FILTERED lines: "
-#   printf '%s\n' "$FFLAG_FILTERED" | wc -l
-#
-    echo "$FFLAG_FILTERED"
+if [ "$VERBOSE" = true ]; then
+    printf '%s\n\n' 'Running with --verbose'
+    printf '%s\n\n' "$FFLAG_FILTERED"
+    printf 'Total Luau FFlags: \t%s\n' "$(printf '%s\n' "$FFLAG_ARG_LINES" | wc -l)"
+    printf 'Compatible FFlags: \t%s\n' "$(printf '%s\n' "$FFLAG_FILTERED" | wc -l)"
+    printf '\nRunning %s...\n' "$LUAU_PATH"
 fi
 
 # Final modification to make usable when executing the binary
 FFLAG_FILTERED=$(printf '%s\n' "$FFLAG_FILTERED" | tr '\n' ',')
 
 # Run luau binary, disable all unspecified fflags, apply FFLAG_FILTERED
-exec "$(command -v $LUAU_BIN)" --fflags="false,$FFLAG_FILTERED" "$@"
+exec "$LUAU_PATH" --fflags="false,$FFLAG_FILTERED" "$@"
